14d01c0cdfea8ab4b1c18c965f39193cef50900f
Soyaaaa081305 <isaiahandreinoda@gmail.com>
2025-10-06 18:35:21 +0800

    Implemented stack class and Towers of Hanoi logic

---
+++ tower_logic.py
@@ -0,0 +1,26 @@
+from node import Node
+
+class Stack:
+    def __init__(self):
+        self.top = None
+    
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+    
+    def pop(self):
+        if not self.is_empty():
+            poppedvalue = self.top.value
+            self.top = self.top.next
+            return poppedvalue
+        return None
+             
+    def is_empty(self):
+        return self.top is None
+    
+    def create_new_stack(self):
+        self.top = None
+    
+    def peek(self):
+        return self.top.value
